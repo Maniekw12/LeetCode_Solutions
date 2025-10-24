ef0b3ab6b0ee46c1cfb05a8efd583a3b6e442611
Marian Wachala <maniekw12w@gmail.com>
2025-10-24 20:52:03 +0200

    add solution to "Remove duplicates " problem

---
+++ Day_4/Remove_Duplicates/Soultion.java
@@ -0,0 +1,31 @@
+package Day_4.Remove_Duplicates;
+
+
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
+
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head == null){
+            return null;
+        }
+        ListNode savhead = head;
+        ListNode curr = head;
+
+        while(curr != null && curr.next != null){
+            if(curr.val == curr.next.val){
+                curr.next = curr.next.next;
+            }
+            else{
+                curr = curr.next; // move cursor only when elemnet was not deleted
+            }
+        }
+
+        return savhead;
+    }
+}
