585242f206e64db7624377b72e84cba678c114ee
Marian Wachala <wachala.marian@gmail.com>
2025-10-28 17:52:10 +0100

    add solution to "Invert Binary Tree" problem

---
+++ Day_6/Invert_Binary_Tree/Solution.java
@@ -0,0 +1,37 @@
+package Day_6.Invert_Binary_Tree;
+import java.util.Stack;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null){
+            return null;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while(!stack.isEmpty()){
+            TreeNode node = stack.pop();
+            TreeNode temp = node.left;
+            node.left = node.right;
+            node.right = temp;
+            if(node.left != null) stack.push(node.left);
+            if(node.right != null) stack.push(node.right);
+
+        }
+        return root;
+    }
+}
